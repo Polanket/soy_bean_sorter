c35ff32bc235c436052e423a87cebfb3497d3a8d
Juan <juanmpt@outlook.com>
2019-08-27 22:54:16 +0200

    Add index and show views + webpack config fix

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,8 @@
-const { environment } = require('@rails/webpacker');
 // config/webpack/environment.js
-    const { environment } = require('@rails/webpacker');
+const { environment } = require('@rails/webpacker');
 
 // Bootstrap 4 has a dependency over jQuery & Popper.js:
-    const webpack = require('webpack');
+const webpack = require('webpack');
     environment.plugins.prepend('Provide',
         new webpack.ProvidePlugin({
             $: 'jquery',
